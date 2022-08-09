98f39d4f1e4f3f9cc897f07fd605b7e2938b0948
Carlos <carlos.e.santaniello@gmail.com>
2022-08-09 16:22:03 +0200

    Agrega conjuntos de Sets

---
+++ CursoJS/Ejercicio7/conjuntos.js
@@ -0,0 +1,10 @@
+const miFamilia = ["Carlos", "Pedro", "Maria", "Luis", "Ana"];
+const miSet = new Set(miFamilia);
+
+console.log(miSet);
+
+miSet.add("Carlos");
+console.log(miSet);
+
+miSet.add("JavaScript");
+console.log(miSet);
